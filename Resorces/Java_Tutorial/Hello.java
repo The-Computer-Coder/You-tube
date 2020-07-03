17b90bd5ac20d3a10761b8829f5cd587ae709a69
Computer coder <67605185+The-Computer-Coder@users.noreply.github.com>
2020-07-03 11:03:32 +0530

    Adding Java tutorial day-2 code

---
+++ Resorces/Java_Tutorial/Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+    public static void main(String[] arg)
+    {
+        System.out.println("Hello Frds");
+        System.out.print("Hello, World!");
+        System.out.printf("%s" , "Computer Coder");
+        System.out.printf("%d" , 100);
+        System.out.printf("%.2f" , 31.4455);
+        /*
+        * %d == > int
+        * %f == > float
+        * %c == > char
+        * %s == >String
+        * */
+    }
+}
