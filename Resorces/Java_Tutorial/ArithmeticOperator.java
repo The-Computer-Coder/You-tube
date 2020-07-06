4cf0763348ddfb959075213c311979162bfe0913
Computer coder <67605185+The-Computer-Coder@users.noreply.github.com>
2020-07-06 16:28:17 +0530

    uploding ArithmeticOperator.java

---
+++ Resorces/Java_Tutorial/ArithmeticOperator.java
@@ -0,0 +1,60 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+        int addtionOfTwoNumber = 200 +200 ;
+        int subtrationOfTwoNumber = 300 - 20;
+        int multipleOfTwoNumber = 300*3;
+        int divitionOfTwoNumber = 500/5;
+        int moduloOfTwoNumber = 800%7 ;
+
+        System.out.println(addtionOfTwoNumber);
+        System.out.println(subtrationOfTwoNumber);
+        System.out.println(multipleOfTwoNumber);
+        System.out.println(divitionOfTwoNumber);
+        System.out.println(moduloOfTwoNumber);
+
+        int incrementnumber = 5;
+        System.out.println(incrementnumber++);
+        System.out.println(++incrementnumber);
+
+        int decrementNumber = 10 ;
+        System.out.println(decrementNumber--);
+        System.out.println(--decrementNumber);
+
+
+        /*Assigment Operators*/
+
+        int variable;
+        variable = 3;
+        variable +=2;
+        variable -=2;
+        variable *=5;
+        variable /=5;
+        variable %=5;
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
