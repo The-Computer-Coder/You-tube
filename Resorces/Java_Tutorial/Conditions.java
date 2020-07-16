108668b88cfe043a9b1af8f4b845577ccd293191
Hari <darkinternet0@gmail.com>
2020-07-16 18:42:46 +0530

    Conditions.java

---
+++ Resorces/Java_Tutorial/Conditions.java
@@ -0,0 +1,101 @@
+class Conditions{
+    public static void main(String[] args) {
+
+        /*int evenNUmber = 101;
+        boolean isTrue = evenNUmber %2 == 0 ;
+
+        if(isTrue){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd Number");
+        }*/
+
+        /*int firstNumber ;
+        int secondNumber ;
+        int thirdNumber ;
+        firstNumber = 100;
+        secondNumber = 200;
+        thirdNumber = 250;
+
+        int result ;
+
+        if(firstNumber > secondNumber && firstNumber > secondNumber)
+            result = firstNumber ;
+        else if(secondNumber > thirdNumber)
+            result = secondNumber;
+        else
+            result = thirdNumber;
+
+        System.out.println(result);*/
+
+            //      Switch Case Statements
+
+        /*
+         *   switch(value)
+         * {
+         *       case 1:
+         *              //Code to be excuted
+         *               break;
+         *       case 2:
+         *              //Code to be excuted
+         *               break;
+         *      .
+         *      .
+         *      .
+         *       case n:
+         *              //Code to be excuted
+         *               break;
+         *      default :
+         *              //Code to be excuted
+         *  }
+        * */
+
+        int date = 3;
+
+        switch (date){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Unknown Day");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
