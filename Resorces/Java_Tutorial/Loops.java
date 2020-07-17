8434ec17aec2f5165cb02265566cc2d505246734
Hari <darkinternet0@gmail.com>
2020-07-17 18:04:16 +0530

    Loop.java is the file Contains java loop. addiung it to git

---
+++ Resorces/Java_Tutorial/Loops.java
@@ -0,0 +1,60 @@
+/*  Loops
+*
+*   1)do..while()
+*   do{
+*           //code to be excuted
+*   }while(Conditions)
+*
+* 2) while()
+* while(conditions)
+* {
+*           //code to be excuted
+*   }
+*
+*  3) for loop
+*   for(init ; conditions ; inc/dec){
+*       //code to be excuted
+*   }
+*
+* */
+
+class Loops{
+    public static void main(String[] args) {
+        /*int loopVar = 0;
+        do{
+            loopVar++;
+            System.out.println(loopVar);
+
+        }while (loopVar <=5);
+
+        loopVar  = 0;
+        while (loopVar <= 5)
+        {
+
+            System.out.println(loopVar);
+            loopVar++;
+        }*/
+
+                        /*For loop*/
+        int loopVar ;
+
+        for(loopVar = 0 ; loopVar <= 5 ; loopVar++){
+            System.out.println(loopVar);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
